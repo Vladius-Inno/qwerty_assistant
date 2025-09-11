4a1f106a03c53e3424b1a3a35bb9eac6313d9dc9
Vladius <deathblow.vlad@gmail.com>
2025-09-11 09:11:12 +0300

    fix_db_connect

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DB_DSN = os.getenv("DATABASE_URL") or "postgresql://qwerty:qwerty@77.37.96.71:5432/archive_db"
+DB_DSN = os.getenv("DATABASE_URL")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "text-embedding-3-small")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
